76b6802a5392b8545cfb2f5d14ca3e1ee3f8aeed
chandisarandeni <chandisarandeni2003@gmail.com>
2025-11-08 19:18:16 +0530

    feat: implement income basic ✅

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { IncomeModule } from './income/income.module';
 
 @Module({
-  imports: [],
+  imports: [IncomeModule],
   controllers: [AppController],
   providers: [AppService],
 })
