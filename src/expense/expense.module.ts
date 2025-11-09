105267aeabe3bd5f746f49762eaac5e3754838a6
chandisarandeni <chandisarandeni2003@gmail.com>
2025-11-09 18:48:30 +0530

    feat: implement expense crud baisc✅

--- src/expense/expense.module.ts
+++ src/expense/expense.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ExpenseService } from './expense.service';
 import { ExpenseController } from './expense.controller';
+import { Expense } from './entities/expense.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Expense])],
   controllers: [ExpenseController],
   providers: [ExpenseService],
 })
