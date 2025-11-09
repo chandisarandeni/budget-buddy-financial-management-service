084a8263e1e48352dcf96ce21ddfccc3425bea8d
chandisarandeni <chandisarandeni2003@gmail.com>
2025-11-09 21:26:22 +0530

    fix: module import error ✅

--- src/budget/budget.module.ts
+++ src/budget/budget.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BudgetService } from './budget.service';
 import { BudgetController } from './budget.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Budget } from './entities/budget.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Budget])],
   controllers: [BudgetController],
   providers: [BudgetService],
 })
